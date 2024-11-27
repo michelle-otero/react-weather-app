3c064bac73f193c6b0b19428582a01d664364e14
Metasilentium <michelleoteroavila@gmail.com>
2024-11-27 13:11:13 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
     <div className="App">
       <div className="weather-app">
         <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/michelle-otero-6919b3171/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Michelle Otero
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/michelle-otero/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source
+          </a>
+        </footer>
       </div>
     </div>
   );
