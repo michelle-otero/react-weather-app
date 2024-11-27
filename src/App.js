1544f479f284c60744a3b3f13aa92a36bf7fac17
Metasilentium <michelleoteroavila@gmail.com>
2024-11-27 13:06:46 -0500

    created react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="weather-app">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
